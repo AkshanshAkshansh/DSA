d8307066d77bb9c44c464088624f762ad2ad264d
unknown <Akshansh Singh>
2023-02-13 00:51:19 +0530

    Sort 0's and 1's using two pointer

---
+++ Recusion/PrintN_To_1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    if(n==1){
+        cout << 1 << " ";
+        return;
+    }
+    cout << n << " ";
+    print(n-1);
+}
+
+int main(){
+    print(5);
+    return 0;
+}
